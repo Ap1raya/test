4546b897f332fb3bb8696df515c062ab9aa437b2
Ap1raya <saynakhax@gmail.com>
2025-08-27 13:36:52 +0700

    test

--- part2-react-v3/vite.config.js
+++ part2-react-v3/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
+  build: {
+    outDir: 'dist-V3',
+    assetsDir: 'assets',
+    sourcemap: false, // ปิดสำหรับ production
+  }
 })
